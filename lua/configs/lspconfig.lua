ff8d340f9e453e2d0677204c34dfb059ca65499c
Satanx016 <z.dadsi21@gmail.com>
2024-04-28 15:04:46 +0100

    Refactor

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,11 +1,11 @@
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 local servers = { "html", "cssls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = lspconfig.on_attach,
-    on_init = lspconfig.on_init,
-    capabilities = lspconfig.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = lspconfig.on_attach,
+		on_init = lspconfig.on_init,
+		capabilities = lspconfig.capabilities,
+	})
 end
