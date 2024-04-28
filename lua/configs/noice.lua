cda9fa3b90a4b10ba5277a87405855d73d274ba5
Satanx016 <z.dadsi21@gmail.com>
2024-04-28 17:38:46 +0100

    Added terminal resizing while in terminal mode

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -3,8 +3,6 @@ local options = {
 		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["config.lsp.signature.enabled"] = false,
-			["config.lsp.hover.enabled"] = false,
 			["vim.lsp.util.stylize_markdown"] = true,
 			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 		},
@@ -15,7 +13,7 @@ local options = {
 		command_palette = true, -- position the cmdline and popupmenu together
 		long_message_to_split = true, -- long messages will be sent to a split
 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = false, -- add a border to hover docs and signature help
+		lsp_doc_border = true, -- add a border to hover docs and signature help
 	},
 }
 
