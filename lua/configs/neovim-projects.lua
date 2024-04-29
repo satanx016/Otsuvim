021a10b150af2b63e7586ac6331ea68db61a3675
Satanx016 <z.dadsi21@gmail.com>
2024-04-30 00:24:00 +0100

    added config for plugin

---
+++ lua/configs/neovim-projects.lua
@@ -0,0 +1,11 @@
+local options = {
+	projects = { -- define project roots
+		"~/projects/*",
+		"~/dotfiles/*",
+		"~/dotfiles/.config/*",
+	},
+	last_session_on_startup = false,
+	dashboard_mode = true,
+}
+
+return options
