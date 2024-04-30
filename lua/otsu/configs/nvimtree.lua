39c301e97ae08cf0737622e7dde6d0ad82925536
Satanx016 <z.dadsi21@gmail.com>
2024-04-30 13:05:22 +0100

    added: flexible mappings, LSP Diagnostics

--- lua/otsu/configs/nvimtree.lua
+++ lua/otsu/configs/nvimtree.lua
@@ -23,6 +23,9 @@ local options = {
 			resize_window = true,
 		},
 	},
+	diagnostics = {
+		enable = true,
+	},
 	renderer = {
 		root_folder_label = false,
 		highlight_git = true,
@@ -66,6 +69,18 @@ local options = {
 		},
 	},
 }
+
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+	api.config.mappings.default_on_attach(bufnr)
+
+	vim.keymap.set("n", "<C-[>", api.tree.change_root_to_parent, opts("CD"))
+end
+
+options.on_attach = on_attach
 
 local config = require("nvconfig").ui.nvimtree
 
