fc2b9020603eca581403594b81facda73a674775
Satanx016 <z.dadsi21@gmail.com>
2024-05-13 17:27:08 +0100

    mappings: formatted file, removed unnecessary terminal toggle func args

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -56,17 +56,17 @@ map(n, "<leader>fc", "<cmd>Telescope git_commits<CR>", { desc = "Git Commits" })
 map(n, "<leader>fC", "<cmd>Telescope git_bcommits<CR>", { desc = "Git Current buffer commits" })
 map(n, "<leader>gg", "<cmd>Telescope git_status<CR>", { desc = "Git Status" })
 
-map(n, "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", {desc = "Git Preview hunk"})
-map(n, "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", {desc = "Git Stage hunk"})
-map(n, "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>", {desc = "Git Undo stage hunk"})
-map(n, "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", {desc = "Git Reset hunk"})
-map(n, "<leader>gS", "<cmd>Gitsigns stage_buffer<CR>", {desc = "Git Stage buffer"})
-map(n, "<leader>gR", "<cmd>Gitsigns reset_buffer<CR>", {desc = "Git Reset buffer"})
-map(n, "<leader>gb", "<cmd>Gitsigns blame_line<CR>", {desc = "Git Blame line"})
-map(n, "<leader>gd", "<cmd>Gitsigns toggle_deleted<CR>", {desc = "Git Toggle deleted"})
-map(n, "<leader>gB", "<cmd>Gitsigns toggle_current_line_blame<CR>", {desc = "Git Toggle line blame"})
+map(n, "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Git Preview hunk" })
+map(n, "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", { desc = "Git Stage hunk" })
+map(n, "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>", { desc = "Git Undo stage hunk" })
+map(n, "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Git Reset hunk" })
+map(n, "<leader>gS", "<cmd>Gitsigns stage_buffer<CR>", { desc = "Git Stage buffer" })
+map(n, "<leader>gR", "<cmd>Gitsigns reset_buffer<CR>", { desc = "Git Reset buffer" })
+map(n, "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "Git Blame line" })
+map(n, "<leader>gd", "<cmd>Gitsigns toggle_deleted<CR>", { desc = "Git Toggle deleted" })
+map(n, "<leader>gB", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Git Toggle line blame" })
 
-map(n, "<leader>gD", "<cmd>DiffviewOpen<CR>", {desc = "Git Diffview open"})
+map(n, "<leader>gD", "<cmd>DiffviewOpen<CR>", { desc = "Git Diffview open" })
 
 -- sessions/projects
 map(n, "<leader>pf", "<cmd>Telescope neovim-project discover<CR>", { desc = "Project List all" })
@@ -104,7 +104,7 @@ map(n, "<leader>boq", function()
 end, { desc = "Buffer Close Others" })
 
 map(n, "<leader>baq", function()
-    vim.cmd("Nvdash")
+	vim.cmd("Nvdash")
 	require("otsu.tabufline").closeOtherBufs()
 end, { desc = "Buffer Close All" })
 
@@ -162,15 +162,15 @@ end, { desc = "Terminal Close term" })
 
 -- toggleable terminals
 map(nt, "<A-v>", function()
-	require("otsu.term").toggle({ pos = "vsp", id = "vtoggleTerm", size = 0.3 })
+	require("otsu.term").toggle({ pos = "vsp", id = "vtoggleTerm"})
 end, { desc = "Terminal Toggleable vertical term" })
 
 map(nt, "<A-s>", function()
-	require("otsu.term").toggle({ pos = "sp", id = "htoggleTerm", size = 0.3 })
+	require("otsu.term").toggle({ pos = "sp", id = "htoggleTerm"})
 end, { desc = "Terminal Toggleable horizontal term" })
 
 map(nt, "<A-t>", function()
-	require("otsu.term").toggle({ pos = "float", id = "floatTerm" })
+	require("otsu.term").toggle({ pos = "float", id = "floatTerm"})
 end, { desc = "Terminal Toggle floating term" })
 
 -- whichkey
