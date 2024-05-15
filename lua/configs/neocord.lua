5033831c35ade6d96150b0542578f150224e10e0
Satanx016 <z.dadsi21@gmail.com>
2024-05-15 13:13:35 +0100

    neocord: better discord rich presence text display :3

--- lua/configs/neocord.lua
+++ lua/configs/neocord.lua
@@ -16,6 +16,15 @@ local options = {
 	show_time = true,
 	global_timer = true,
 	-- enable_line_number = 1,
+
+    workspace_text      = "Working on %s",          -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+	editing_text        = "Adding bugs to %s",      -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+	reading_text        = "O_O Staring at %s",          -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text  = "ಠ_ಠ Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    git_commit_text     = ">_< Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+    plugin_manager_text = "⌬ Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+    terminal_text       = "Using Terminal",           -- Format string rendered when in terminal mode.
 }
 
 require("neocord").setup(options)
