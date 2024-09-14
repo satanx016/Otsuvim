740ec7bb1f197b7b4de663ea1c3649fb61e968c4
Satanx016 <z.dadsi21@gmail.com>
2024-09-14 16:33:33 +0100

    style: format code

--- init.lua
+++ init.lua
@@ -7,8 +7,8 @@ _G.Otsuvim = require("otsuvim.util")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
