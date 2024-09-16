7c5bf87553ed91597c55b7083c7098a21cd15e06
Satanx016 <z.dadsi21@gmail.com>
2024-09-16 07:15:56 +0100

    feat(gitsigns): add new icons

--- lua/otsuvim/plugins/git.lua
+++ lua/otsuvim/plugins/git.lua
@@ -5,12 +5,12 @@ return {
 		opts = function()
 			return {
 				signs = {
-					add = { text = "│" },
-					change = { text = "│" },
-					delete = { text = "󰍵" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "│" },
+					add = { text = "▎" },
+					change = { text = "▎" },
+					delete = { text = "" },
+					topdelete = { text = "" },
+					changedelete = { text = "▎" },
+					untracked = { text = "▎" },
 				},
 				signs_staged_enable = false,
 			}
