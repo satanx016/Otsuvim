e028fd74d695ea967c107bb97e2f5ff56fe0c8a4
Satanx016 <z.dadsi21@gmail.com>
2024-09-19 22:06:57 +0100

    refactor: removing dead code

--- lua/otsuvim/plugins/lsp/signature.lua
+++ lua/otsuvim/plugins/lsp/signature.lua
@@ -1,16 +1,3 @@
--- Conflicts with noice.nvim (Wrong behavior of handlers when using with)
-
--- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
---     border = "rounded",
--- })
---
--- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
---   border = "rounded",
---   focusable = false,
---   relative = "cursor",
---   silent = true,
--- })
-
 local M = {}
 
 M.check_triggeredChars = function(triggerChars)
