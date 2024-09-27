ffb8a3e527c545f72a93335841bc3f59fe14a293
Satanx016 <z.dadsi21@gmail.com>
2024-09-27 16:35:51 +0100

    refactor(noice): remove unneeded comments and opts

--- lua/otsuvim/plugins/ui.lua
+++ lua/otsuvim/plugins/ui.lua
@@ -47,7 +47,6 @@ return {
 		opts = function()
 			return {
 				lsp = {
-					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 					override = {
 						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 						["vim.lsp.util.stylize_markdown"] = true,
@@ -56,11 +55,10 @@ return {
 				},
 
 				presets = {
-					bottom_search = false, -- use a classic bottom cmdline for search
-					command_palette = true, -- position the cmdline and popupmenu together
-					long_message_to_split = true, -- long messages will be sent to a split
-					inc_rename = false, -- enables an input dialog for inc-rename.nvim
-					lsp_doc_border = true, -- add a border to hover docs and signature help
+					bottom_search = false,
+					command_palette = true,
+					long_message_to_split = true,
+					lsp_doc_border = true,
 				},
 			}
 		end,
