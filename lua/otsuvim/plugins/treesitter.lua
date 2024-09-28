34ffdd21d0d8328f15650e13a091fcd309e72b05
Satanx016 <z.dadsi21@gmail.com>
2024-09-28 03:38:47 +0100

    feat(treesitter): remove defaults and add incremental selection

--- lua/otsuvim/plugins/treesitter.lua
+++ lua/otsuvim/plugins/treesitter.lua
@@ -14,14 +14,16 @@ return {
 					"vim",
 					"vimdoc",
 				},
-				auto_install = true,
-
-				highlight = {
-					enable = true,
-					use_languagetree = true,
-				},
-
 				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<A-space>",
+					},
+				},
 			}
 		end,
 		config = function(_, opts)
