884dbde8b77029f8c2785e2ff15677e6d033e27c
Satanx016 <z.dadsi21@gmail.com>
2024-09-30 07:33:53 +0100

    style: remove space

--- lua/otsuvim/plugins/editor.lua
+++ lua/otsuvim/plugins/editor.lua
@@ -108,7 +108,7 @@ return {
 		"folke/todo-comments.nvim",
 		cmd = { "TodoTelescope" },
 		event = "LazyFile",
-    opts = {},
+		opts = {},
     -- stylua: ignore
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
