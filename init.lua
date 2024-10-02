a402ed19a9f123bc375d25b83bc1f7abecccfb0c
Satanx016 <z.dadsi21@gmail.com>
2024-10-02 13:54:14 +0100

    refactor(lazy): migrate lazy bootstrapping from `init.lua` to `otsuvim.util.lazy.lua`

--- init.lua
+++ init.lua
@@ -1,22 +1,9 @@
 vim.g.based_cache = vim.fn.stdpath("data") .. "/otsu-ui/based/"
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-	local repo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("otsuvim.config.options")
 _G.Otsuvim = require("otsuvim.util")
-Otsuvim.lazy.setup()
 
-local lazy_config = require("otsuvim.config.lazy")
-require("lazy").setup({
-	{ import = "otsuvim.plugins" },
-	{ import = "otsuvim.plugins.extras.vanity" },
-	-- { import = "otsuvim.plugins.test" },
-}, lazy_config)
+Otsuvim.lazy.setup()
 
 dofile(vim.g.based_cache .. "defaults")
 dofile(vim.g.based_cache .. "statusline")
--- lua/otsuvim/util/lazy.lua
+++ lua/otsuvim/util/lazy.lua
@@ -1,7 +1,20 @@
 local M = {}
 
 M.setup = function()
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.uv.fs_stat(lazypath) then
+		local repo = "https://github.com/folke/lazy.nvim.git"
+		vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+	end
+	vim.opt.rtp:prepend(lazypath)
+
 	M.lazy_file()
+
+	local lazy_config = require("otsuvim.config.lazy")
+	require("lazy").setup({
+		{ import = "otsuvim.plugins" },
+		{ import = "otsuvim.plugins.extras.vanity" },
+	}, lazy_config)
 end
 
 M.lazy_file = function()
