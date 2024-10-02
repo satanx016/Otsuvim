6c90780cafa7e70d73dc1cedc2e2723fdfcc6526
Satanx016 <z.dadsi21@gmail.com>
2024-10-02 16:06:42 +0100

    refactor(dead-code): these autocmds are already included in `otsu-ui.git`

--- lua/otsuvim/config/autocmds.lua
+++ lua/otsuvim/config/autocmds.lua
@@ -1,11 +1,1 @@
-local autocmd = vim.api.nvim_create_autocmd
-
--- Help/Man Veritcal split
-autocmd("BufWinEnter", {
-	pattern = { "*.txt", "*(*)" },
-	callback = function()
-		if vim.bo.filetype == "help" or vim.bo.filetype == "man" then
-			vim.cmd("wincmd L")
-		end
-	end,
-})
+-- Auto-commands here
