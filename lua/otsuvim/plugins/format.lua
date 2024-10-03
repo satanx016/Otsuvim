8169fcbb99ef4ddf5e4d3b10c6db5dca2ba8063a
Satanx016 <z.dadsi21@gmail.com>
2024-10-03 22:14:37 +0100

    style(format): remove verbose comments

--- lua/otsuvim/plugins/format.lua
+++ lua/otsuvim/plugins/format.lua
@@ -4,11 +4,8 @@ return {
 		event = "LazyFile",
 		opts = function()
 			return {
-				-- Conform will run multiple formatters sequentially / Use a sub-list to run only the first available formatter
 				formatters_by_ft = {
 					lua = { "stylua" },
-					-- python = { "isort", "black" },
-					-- javascript = { { "prettierd", "prettier" } },
 				},
 
 				format_on_save = function()
