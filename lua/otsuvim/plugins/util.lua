5381a143904f9a120548e129a18559c037417b8b
Satanx016 <z.dadsi21@gmail.com>
2024-11-22 03:33:23 +0100

    fix(neorg): fix replaced based's neorg highlights

--- lua/otsuvim/plugins/util.lua
+++ lua/otsuvim/plugins/util.lua
@@ -42,21 +42,19 @@ return {
       return {
         load = {
           ["core.defaults"] = {},
-          ["core.concealer"] = {
-            config = {
-              icon_preset = "diamond",
-            },
-          },
+          ["core.summary"] = {},
+          ["core.concealer"] = { config = { icon_preset = "diamond" } },
           ["core.dirman"] = {
             config = {
-              workspaces = {
-                notes = "~/notes",
-              },
+              workspaces = { notes = "~/notes" },
               default_workspace = "notes",
             },
           },
-          ["core.summary"] = {},
           ["core.completion"] = { config = { engine = "nvim-cmp" } },
+          ["core.highlights"] = {
+            config = { highlights = { todo_items = { on_hold = "+@neorg.todo_items.on_hold.norg" } } },
+          },
+
           ["external.neorg-colorizer"] = {},
         },
       }
