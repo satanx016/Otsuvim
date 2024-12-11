9b0b088a13498b9084fc8e79d832c71a7e8b6ab1
Satanx016 <z.dadsi21@gmail.com>
2024-12-11 04:11:03 +0100

    style(todo-comments): remove the weird `'s`

--- lua/otsuvim/plugins/editor.lua
+++ lua/otsuvim/plugins/editor.lua
@@ -136,7 +136,7 @@ return {
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo's" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
     },
     opts = {
       keywords = {
