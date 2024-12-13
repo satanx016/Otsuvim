4c93b43dcb7e119accf421798ef8bb27c6bb496a
Satanx016 <z.dadsi21@gmail.com>
2024-12-13 15:34:08 +0100

    fix(notify): fix notification taking too much space

--- lua/otsuvim/plugins/ui.lua
+++ lua/otsuvim/plugins/ui.lua
@@ -34,7 +34,7 @@ return {
         timeout = 3700,
         -- stylua: ignore start
         max_height = function() return math.floor(vim.o.lines * 0.5) end,
-        max_width = function() return math.floor(vim.o.columns * 0.6) end,
+        max_width = function() return math.floor(vim.o.columns * 0.4) end,
         icons = { ERROR = "󰅙 ", WARN = " ", INFO = " ", DEBUG = " ", TRACE = " " },
         -- stylua: ignore end
       }
