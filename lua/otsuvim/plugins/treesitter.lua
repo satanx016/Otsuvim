51af7dc2e80146ca9145f9aadfe2c69e34d0032d
Satanx016 <z.dadsi21@gmail.com>
2024-12-15 19:01:48 +0100

    feat(mini.ai): hehe... enhancing textobjects

--- lua/otsuvim/plugins/treesitter.lua
+++ lua/otsuvim/plugins/treesitter.lua
@@ -37,59 +37,6 @@ return {
             node_decremental = "<A-space>",
           },
         },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["al"] = { query = "@loop.outer", desc = "loop" },
-              ["il"] = { query = "@loop.inner", desc = "inner loop" },
-              ["ab"] = { query = "@block.outer", desc = "block" },
-              ["ib"] = { query = "@block.inner", desc = "inner block" },
-              ["af"] = { query = "@function.outer", desc = "function" },
-              ["if"] = { query = "@function.inner", desc = "inner function" },
-              ["ac"] = { query = "@conditional.outer", desc = "condition" },
-              ["ic"] = { query = "@conditional.inner", desc = "inner condition" },
-              ["ap"] = { query = "@parameter.outer", desc = "parameter" },
-              ["ip"] = { query = "@parameter.inner", desc = "inner parameter" },
-              ["av"] = { query = "@assignment.lhs", desc = "variable" },
-              ["aa"] = { query = "@assignment.rhs", desc = "assignment" },
-              ["a/"] = { query = "@comment.outer", desc = "comment" },
-              ["i/"] = { query = "@comment.inner", desc = "inner comment" },
-            },
-          },
-          move = {
-            enable = true,
-            goto_next_start = {
-              ["]f"] = { query = "@function.outer", desc = "Next function start" },
-              ["]c"] = { query = "@conditional.outer", desc = "Next condition start" },
-              ["]p"] = { query = "@parameter.inner", desc = "Next parameter start" },
-              ["]v"] = { query = "@assignment.lhs", desc = "Next variable start" },
-              ["]a"] = { query = "@assignment.rhs", desc = "Next assignment start" },
-            },
-            goto_next_end = {
-              ["]F"] = { query = "@function.outer", desc = "Next function end" },
-              ["]C"] = { query = "@conditional.outer", desc = "Next condition end" },
-              ["]P"] = { query = "@parameter.inner", desc = "Next parameter end" },
-              ["]V"] = { query = "@assignment.lhs", desc = "Next variable end" },
-              ["]A"] = { query = "@assignment.rhs", desc = "Next assignment end" },
-            },
-            goto_previous_start = {
-              ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-              ["[c"] = { query = "@conditional.outer", desc = "Previous condition start" },
-              ["[p"] = { query = "@parameter.inner", desc = "Previous parameter start" },
-              ["[v"] = { query = "@assignment.lhs", desc = "Previous variable start" },
-              ["[a"] = { query = "@assignment.rhs", desc = "Previous assignment start" },
-            },
-            goto_previous_end = {
-              ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-              ["[C"] = { query = "@conditional.outer", desc = "Previous condition end" },
-              ["[P"] = { query = "@parameter.inner", desc = "Previous parameter end" },
-              ["[V"] = { query = "@assignment.lhs", desc = "Previous variable end" },
-              ["[A"] = { query = "@assignment.rhs", desc = "Previous assignment end" },
-            },
-          },
-        },
       }
     end,
     config = function(_, opts)
@@ -102,6 +49,31 @@ return {
       vim.filetype.add({
         pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
       })
+    end,
+  },
+
+  {
+    "echasnovski/mini.ai",
+    event = "LazyFile",
+    opts = function()
+      local ai = require("mini.ai")
+
+      return {
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }), -- code block
+
+          ["="] = ai.gen_spec.treesitter({ a = { "@assignment.rhs" }, i = { "@assignment.rhs" } }), -- assignment
+          a = ai.gen_spec.argument({ brackets = { "%b()" }, separator = ";" }), -- argument
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+          d = ai.gen_spec.treesitter({ a = "@number.inner", i = "@number.inner" }), -- number
+          g = Otsuvim.mini.gen_spec_buffer(),
+        },
+        n_lines = 150,
+      }
     end,
   },
 }
---
+++ lua/otsuvim/util/mini.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.gen_spec_buffer()
+  local from = { line = 1, col = 1 }
+  local to = {
+    line = vim.fn.line("$"),
+    col = math.max(vim.fn.getline("$"):len(), 1),
+  }
+  return { from = from, to = to }
+end
+
+return M
