7533a718fcf7a4dbf6dc028606cb8edc01e0c9ef
Satanx016 <z.dadsi21@gmail.com>
2024-12-15 21:38:40 +0100

    fix(mini.ai): fix comment typo

--- lua/otsuvim/plugins/treesitter.lua
+++ lua/otsuvim/plugins/treesitter.lua
@@ -71,7 +71,7 @@ return {
           c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
           d = { "%f[%d]%d+" }, -- digits
           g = Otsuvim.mini.gen_spec_buffer(),
-          e = { -- camel case
+          e = { -- snake/camel case
             { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
             "^().*()$",
           },
