bc4e20e233d71ab4b605ae3f5fe91c62ffcbe8a4
Satanx016 <z.dadsi21@gmail.com>
2024-12-19 02:41:25 +0100

    feat!(mini.surround): wohoo

--- lua/otsuvim/plugins/coding.lua
+++ lua/otsuvim/plugins/coding.lua
@@ -110,4 +110,23 @@ return {
       }
     end,
   },
+
+  {
+    "echasnovski/mini.surround",
+    event = "LazyFile",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+    config = function(_, opts)
+      require("mini.surround").setup(opts)
+    end,
+  },
 }
