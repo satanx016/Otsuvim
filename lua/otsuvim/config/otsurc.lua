60ed2ad8706ba08e6b9d90043803d1b0e10f8558
Satanx016 <z.dadsi21@gmail.com>
2024-12-24 19:08:54 +0100

    feat(dash): add notes to the party!

--- lua/otsuvim/config/otsurc.lua
+++ lua/otsuvim/config/otsurc.lua
@@ -63,9 +63,9 @@ M.ui = {
 
     buttons = {
       { icon = " ", key = "f", txt = "Find File", action = "Telescope find_files" },
-      { icon = " ", key = "n", txt = "New File", action = "ene | startinsert" },
       { icon = " ", key = "r", txt = "Recent Files", action = "Telescope oldfiles" },
       { icon = "󰙅 ", key = "p", txt = "Find Project", action = "NeovimProjectDiscover" },
+      { icon = " ", key = "n", txt = "Notes", action = "Neorg index" },
       { icon = "󰒓 ", key = "c", txt = "Config", action = "Telescope find_files cwd=" .. vim.fn.stdpath("config") },
       { icon = " ", key = "l", txt = "Last project", action = "NeovimProjectLoadRecent" },
       { icon = " ", key = "q", txt = "Quit", action = "qa!" },
