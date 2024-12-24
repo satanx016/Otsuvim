6356530abc80f7e397de1916f335a80a14da2ac3
Satanx016 <z.dadsi21@gmail.com>
2024-12-24 18:26:56 +0100

    fix(cmp): change the confirm_keymap (cause I'm risking RSI with that <C-y>)
    Cuz my laptop keyboard layout doesn't help :/

--- lua/otsuvim/plugins/coding.lua
+++ lua/otsuvim/plugins/coding.lua
@@ -93,7 +93,7 @@ return {
           ["<C-d>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<ESC>"] = cmp.mapping.close(),
-          ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+          ["<C-x>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
           ["<C-n>"] = Otsuvim.cmp.select_next(),
           ["<C-p>"] = Otsuvim.cmp.select_prev(),
         },
