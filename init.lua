1d105b89673b09af624a61530e0cedc050dba7f7
Satanx016 <z.dadsi21@gmail.com>
2024-12-24 04:56:58 +0100

    fix(options): some opts are omitted during early plugins startups

--- init.lua
+++ init.lua
@@ -3,11 +3,12 @@ vim.g.based_cache = vim.fn.stdpath("data") .. "/otsu-ui/based/"
 _G.Otsuvim = require("otsuvim.util")
 Otsuvim.config = require("otsuvim.config.otsurc")
 
+require("otsuvim.config.options")
+
 require("otsuvim.config.lazy").setup()
 
 vim.api.nvim_create_autocmd("UIEnter", {
   callback = function()
-    require("otsuvim.config.options")
     require("otsuvim.config.keymaps")
   end,
 })
