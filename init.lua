374e9489bae8ff693e3ba2c4b3c8df47e96f65b4
Satanx016 <z.dadsi21@gmail.com>
2024-12-27 17:38:54 +0100

    fix(inspect): use correct default highlights

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- NOTE: Temp fix (delete on rebuild:release-0.10)
+-- https://github.com/neovim/neovim/pull/31676
+vim.hl = vim.highlight
+
 vim.g.based_cache = vim.fn.stdpath("data") .. "/otsu-ui/based/"
 
 _G.Otsuvim = require("otsuvim.util")
