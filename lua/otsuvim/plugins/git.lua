50b1e5b5384464b0b31330221a72fcb332f43b47
Satanx016 <z.dadsi21@gmail.com>
2025-01-06 02:29:11 +0100

    fix(gitsigns): move some hunk mappings to the `<leader>gh` group

--- lua/otsuvim/plugins/git.lua
+++ lua/otsuvim/plugins/git.lua
@@ -33,8 +33,8 @@ return {
           map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage hunk")
           map("n", "<leader>ghS", gs.stage_buffer, "Stage buffer")
           map("n", "<leader>ghR", gs.reset_buffer, "Reset buffer")
-          map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame line")
-          map("n", "<leader>gd", gs.diffthis, "Diff this")
+          map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame line")
+          map("n", "<leader>ghd", gs.diffthis, "Diff this")
           -- stylua: ignore end
 
           map({ "o", "x" }, "ah", "<Esc><cmd>Gitsigns select_hunk<CR>", "hunk")
