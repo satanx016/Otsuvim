0934cab25c895f86bf54af1eb437db5564ad47f1
Satanx016 <z.dadsi21@gmail.com>
2025-01-07 23:04:04 +0100

    perf(nvim-lint): debounce linting

--- lua/otsuvim/plugins/linting.lua
+++ lua/otsuvim/plugins/linting.lua
@@ -16,9 +16,19 @@ return {
     config = function(_, opts)
       require("lint").linters_by_ft = opts.linters_by_ft
 
+      local debounce = nil
       vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost" }, {
+        group = vim.api.nvim_create_augroup("nvim-lint", {}),
         callback = function()
-          require("lint").try_lint()
+          if debounce then
+            vim.fn.timer_stop(debounce)
+          end
+          debounce = vim.fn.timer_start(
+            200,
+            vim.schedule(function()
+              require("lint").try_lint()
+            end)
+          )
         end,
       })
     end,
