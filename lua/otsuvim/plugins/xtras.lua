cd7cbb5b2980ed1ed8c9b0e678fc10b385deb317
Satanx016 <z.dadsi21@gmail.com>
2025-01-08 23:36:02 +0100

    refactor(xtras): rename `imports` -> `extras`

--- lua/otsuvim/plugins/xtras.lua
+++ lua/otsuvim/plugins/xtras.lua
@@ -1,12 +1,12 @@
 local config = Otsuvim.config.plugins.extras or {}
-local imports = {}
+local extras = {}
 
 for category, plugins in pairs(config) do
   for plugin, enabled in pairs(plugins) do
     if enabled then
-      table.insert(imports, { import = "otsuvim.plugins.extras." .. category .. "." .. plugin })
+      table.insert(extras, { import = "otsuvim.plugins.extras." .. category .. "." .. plugin })
     end
   end
 end
 
-return imports
+return extras
