54eb7fd536c45d878c9b747a2350ecdef26dfc67
Satanx016 <z.dadsi21@gmail.com>
2025-01-11 20:13:23 +0100

    feat(lsp): proper support for scss/sass

--- lua/otsuvim/plugins/extras/lang/css.lua
+++ lua/otsuvim/plugins/extras/lang/css.lua
@@ -1,10 +1,27 @@
 return {
+
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "prettier" } },
+  },
+
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        cssls = {},
+        cssls = {
+          filetypes = { "css", "less" },
+        },
+        somesass_ls = {},
       },
     },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft.scss = opts.formatters_by_ft.scss or {}
+      table.insert(opts.formatters_by_ft.scss, "prettier")
+    end,
   },
 }
