12202d65a419a0a3c2109ef027b57e241cfdb4ab
zerachiel016 <z.dadsi21@gmail.com>
2026-06-30 20:08:53 +0100

    fix(tree-sitter): major fixes & auto enabling tree-sitter highlights

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,42 @@
 return {
-    {
-        'nvim-mini/mini.ai',
-        event = "LazyFile",
-        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-        opts = function()
-            local ai = require("mini.ai")
-            return {
-                custom_textobjects = {
-                    o = ai.gen_spec.treesitter({ a = { "@conditional.outer" }, i = { "@conditional.inner" } }), -- code block
-                    a = ai.gen_spec.treesitter({ a = { "@parameter.outer" }, i = { "@parameter.inner" } }), -- argument
-                    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
-                    d = { "%f[%d]%d+" }, -- digits
-                    g = { from = { line = 1, col = 1 }, to = { line = vim.fn.line("$"), col = math.max(vim.fn.getline("$"):len(), 1) }} -- entire file
-                },
-                silent = true,
-            }
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+    event = { "LazyFile" },
+    build = ":TSUpdate",
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("custom_treesitter_highlighting", { clear = true }),
+        callback = function(e)
+          pcall(vim.treesitter.start, e.buf)
         end,
-        config = function(_, opts)
-            require("mini.ai").setup(opts)
-            Util.mini.ai_whichkey(opts)
-        end,
-    },
+      })
+    end,
+  },
+
+  {
+    "nvim-mini/mini.ai",
+    event = "LazyFile",
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({ a = { "@conditional.outer" }, i = { "@conditional.inner" } }), -- code block
+          a = ai.gen_spec.treesitter({ a = { "@parameter.outer" }, i = { "@parameter.inner" } }), -- argument
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+          d = { "%f[%d]%d+" }, -- digits
+          g = {
+            from = { line = 1, col = 1 },
+            to = { line = vim.fn.line("$"), col = math.max(vim.fn.getline("$"):len(), 1) },
+          }, -- entire file -- FIX: not working properly
+        },
+        silent = true,
+      }
+    end,
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+      Util.mini.ai_whichkey(opts)
+    end,
+  },
 }
